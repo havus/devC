7552e200c562c5ae96198310464c6808fd20fcce
havus <havus.it@gmail.com>
2019-03-28 21:00:19 +0700

    method math

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+// function komplex() {
+//     var x = document.getElementById('ubah').innerHTML;
+//     if (x == 'Hello World') {
+//         console.log(x);
+//         // tambahContent();
+//     }
+//     gantiContent();
+// }
+
+function tambahContent() {
+    var tittle = document.createElement("h1");
+    tittle.textContent = "Hello World";
+    document.getElementById('ubah').appendChild(tittle);
+}
+
 function gantiContent() {
-    document.getElementById("ubah").textContent = "Hello World";
+    var tittle = document.createElement("h1");
+    var angkaKu = Math.round(21.4);
+    tittle.textContent = "Hello World";
+    document.getElementById("ubah").textContent = angkaKu;
 }
