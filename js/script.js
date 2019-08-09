bba0abfb5b5e5397137d514350576f53f2fb19c6
havus <havus.it@gmail.com>
2019-08-10 00:06:16 +0700

    fix error html

--- js/script.js
+++ js/script.js
@@ -1,21 +1,32 @@
-// function komplex() {
-//     var x = document.getElementById('ubah').innerHTML;
-//     if (x == 'Hello World') {
-//         console.log(x);
-//         // tambahContent();
-//     }
-//     gantiContent();
-// }
+$('.pic-case').css({
+	'opacity': 0,
+	'transition': '.5s'
+})
 
-function tambahContent() {
-    var tittle = document.createElement("h1");
-    tittle.textContent = "Hello World";
-    document.getElementById('ubah').appendChild(tittle);
-}
+$('.smooth-scroll').on('click', function(e) {
+	(e).preventDefault();
+	let distance = $(this).attr('href');
 
-function gantiContent() {
-    var tittle = document.createElement("h1");
-    var angkaKu = Math.round(21.4);
-    tittle.textContent = "Hello World";
-    document.getElementById("ubah").textContent = angkaKu;
-}
+	$([document.documentElement, document.body]).animate({
+		scrollTop: $(distance).offset().top - 100
+	})
+})
+
+$(window).scroll(function() {
+	console.log($(this).scrollTop());
+	let scrollVal = $(this).scrollTop();
+	// 1254
+	if (scrollVal > 1250) {
+		$('.pic-case').each((i) => {
+			setTimeout(() => {
+				$('.pic-case').eq(i).css('opacity', 1);
+			}, 200 * i);
+		})
+	}
+
+	if (scrollVal < 1200) {
+		$('.pic-case').css('opacity', 0);
+		console.log('<<<<<<<<<<<<<<<');
+	}
+
+})
